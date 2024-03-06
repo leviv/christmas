1cd62366d49d8af2e9a49a3018f2abda4f740174
Levi Villarreal <villarreallevi@gmail.com>
2024-03-06 03:28:51 -0500

    initial gh-pages commit

--- gh-pages.js
+++ gh-pages.js
@@ -4,7 +4,7 @@ ghpages.publish(
   "public", // path to public directory
   {
     branch: "gh-pages",
-    repo: "https://github.com/leviv/all-I-want-for-christmas",
+    repo: "https://github.com/leviv/christmas",
     user: {
       name: "Levi Villarreal",
       email: "villarreallevi@gmail.com",
